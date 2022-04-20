05c71c29a55f9e854d1d674e034e16711873ea41
bramvelthoven <bramvelthoven123@gmail.com>
2022-04-20 09:37:46 +0200

    1st

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>@yield('title')</title>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="./css/app.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+    @include('layout.navigation')
+
+    @yield('content')
+
+
+    <script scr="./js/app.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</body>
+
+<footer class="bg-dark text-center text-white mt-auto">
+    @include('layout.footer')
+</footer>
+
+</html>
